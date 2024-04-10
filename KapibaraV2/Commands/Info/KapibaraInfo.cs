460a4de1aa02ce64521caa28ee60cfac2f58bcea
Aleksandr Fomenko <ya@umr3228.ru>
2024-04-11 00:20:31 +0300

    Добавьте файлы проекта.

---
+++ KapibaraV2/Commands/Info/KapibaraInfo.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using KapibaraV2.ViewModels;
+using KapibaraV2.Views;
+using KapibaraV2.Views.Info;
+using Nice3point.Revit.Toolkit.External;
+
+namespace KapibaraV2.Commands
+{
+    /// <summary>
+    ///     External command entry point invoked from the Revit interface
+    /// </summary>
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class KapibaraInfo : ExternalCommand
+    {
+        public override void Execute()
+        {
+            var viewModel = new KapibaraV2ViewModel();
+            var view = new KapibaraInfoView(viewModel);
+            view.ShowDialog();
+        }
+    }
+}
---
+++ KapibaraV2/Views/Info/KapibaraInfoView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using KapibaraV2.ViewModels;
+using System.Diagnostics;
+
+namespace KapibaraV2.Views.Info
+{
+    public sealed partial class KapibaraInfoView
+    {
+        public KapibaraInfoView(KapibaraV2ViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+
+        private void ButtonTelegram(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("https://t.me/AleksandrFomenko228");
+            this.Close();
+        }
+
+        private void ButtonGitHub(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/AleksandrFomenko/KapibaraV2");
+            this.Close();
+
+        }
+    }
+}
