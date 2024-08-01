2c863f67a5f6bb5de76d8efe98bf3eb3ae9f7903
Aleksandr Fomenko <ya@umr3228.ru>
2024-08-01 20:30:28 +0300

    Ðadd solid filter

--- SolidIntersector/ViewModels/SolidIntersectorViewModel.cs
+++ SolidIntersector/ViewModels/SolidIntersectorViewModel.cs
@@ -91,7 +91,7 @@ public partial class SolidIntersectorViewModel : ObservableObject
             })
             .ToList();
     }
-    private List<Element> FindIntersectingElementsBoundingBox(string elementName)
+    private List<Element> FindIntersectingElements(string elementName)
     {
         var element = new FilteredElementCollector(Context.Document)
             .OfCategory(BuiltInCategory.OST_GenericModel)
@@ -104,13 +104,18 @@ public partial class SolidIntersectorViewModel : ObservableObject
             return new List<Element>();
         }
 
+        // Фильтр с BB
         var boundingBox = element.get_BoundingBox(null);
-        
         Outline outline = new Outline(boundingBox.Min, boundingBox.Max);
         BoundingBoxIntersectsFilter filter = new BoundingBoxIntersectsFilter(outline);
 
+        // Фильтр с солидом
+        Solid solid = GetSolid(element);
+        ElementIntersectsSolidFilter solidFilter = new ElementIntersectsSolidFilter(solid);
+        
         var elements = new FilteredElementCollector(Context.Document)
             .WherePasses(filter)
+            .WherePasses(solidFilter)
             .WhereElementIsNotElementType()
             .ToList();
 
@@ -123,7 +128,52 @@ public partial class SolidIntersectorViewModel : ObservableObject
         
         if (selectedItems == null)
         {
-            TaskDialog.Show("Ошибка", "Выберите элемент.");
+            TaskDialog.Show("Ошибка", "Выберите элемент");
         }
+        foreach (Element elem in selectedItems)
+        {
+            List<Element> intersectorItems = FindIntersectingElements(elem.Name);
+        }
+        
+    }
+
+    private Solid GetSolid(Element element)
+    {
+        var opt = new Options
+        {
+            DetailLevel = ViewDetailLevel.Fine
+        };
+        var geometryElement = element.get_Geometry(opt);
+
+        if (geometryElement == null)
+        {
+            return null;
+        }
+
+        foreach (var geomObj in geometryElement)
+        {
+            if (geomObj is Solid solid && solid.Volume > 0)
+            {
+                return solid;
+            }
+            else if (geomObj is GeometryInstance geomInstance)
+            {
+                var instGeom = geomInstance.GetInstanceGeometry();
+                if (instGeom == null)
+                {
+                    continue;
+                }
+
+                foreach (var instGeomObj in instGeom)
+                {
+                    if (instGeomObj is Solid instSolid && instSolid.Volume > 0)
+                    {
+                        return instSolid;
+                    }
+                }
+            }
+        }
+        
+        return null;
     }
 }
